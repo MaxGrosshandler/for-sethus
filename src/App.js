fb03e1ac6deba89771275014cc335a0985835fcd
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-06-23 19:45:54 -0500

    got skeleton working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Progress } from 'react-sweet-progress';
+import 'react-sweet-progress/lib/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      percentage: 20
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    event.preventDefault();
+    console.log(event.currentTarget);
+    this.setState({
+      percentage: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <Progress percent={this.state.percentage} status="success" />
+          <form>
+            <input
+              type="number"
+              name="percentage"
+              onChange={this.handleChange}
+            />
+          </form>
+          <p>here is an example of a progress bar that could be altered</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
